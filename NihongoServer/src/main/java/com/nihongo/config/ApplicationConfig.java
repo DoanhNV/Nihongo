5804f77b0827e75f7c51b83eaf400881bd3e277f
Ngo Van Doanh <doanh.ngo@dahu.vn>
2018-07-10 01:03:16 +0700

    PropertiesChange_20180710_DoanhNV: change host and port

--- NihongoServer/src/main/java/com/nihongo/config/ApplicationConfig.java
+++ NihongoServer/src/main/java/com/nihongo/config/ApplicationConfig.java
@@ -1,8 +1,5 @@
 package com.nihongo.config;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.stereotype.Component;
@@ -12,14 +9,14 @@ public class ApplicationConfig  implements WebServerFactoryCustomizer<Configurab
 
 	@Override
 	public void customize(ConfigurableServletWebServerFactory factory) {
-		factory.setPort(8282);
+	/*	factory.setPort(8282);
 		InetAddress address = null;
 		try {
 			address = InetAddress.getByName("0.0.0.0");
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
-		factory.setAddress(address);
+		factory.setAddress(address);*/
 	}
 
 }
